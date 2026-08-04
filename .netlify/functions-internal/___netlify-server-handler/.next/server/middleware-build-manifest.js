717bf89091dcdb64fdca7cbfb770d4e1c9aa6789
Rechcel Toledo <rechceltoledo@gmail.com>
2026-08-04 19:47:37 +0800

    Fix 2embed false positive health probe & add deep streamsrcs check

--- .netlify/functions-internal/___netlify-server-handler/.next/server/middleware-build-manifest.js
+++ .netlify/functions-internal/___netlify-server-handler/.next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/03~yq9q893hmn.js"
   ],
   "lowPriorityFiles": [
-    "static/XaMhwkpcyVpmOEPfTMblT/_buildManifest.js",
-    "static/XaMhwkpcyVpmOEPfTMblT/_ssgManifest.js",
-    "static/XaMhwkpcyVpmOEPfTMblT/_clientMiddlewareManifest.js"
+    "static/aWk7ZiyRJUFVFqUaYkZly/_buildManifest.js",
+    "static/aWk7ZiyRJUFVFqUaYkZly/_ssgManifest.js",
+    "static/aWk7ZiyRJUFVFqUaYkZly/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/154yue4_t6fan.js",
